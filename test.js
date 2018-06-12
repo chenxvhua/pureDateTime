6d6526efd4777fcbb8af100b05c4a3ccfd6919c7
chenxuhua <chenxvhua@mobanker.com>
2018-06-12 10:39:07 +0800

    feat:all

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var d = new Date("2018-5-18 03:1:1:111");
 console.log(d.getTime())
 
 var dateStr = pureDateTime.formatDate(d,"yyyy-MM-dd HH:mm:ss 第q季"); //24小时，所有是H
-console.log(dateStr); //2018-05-18 03:01:01 第2季
+console.log(dateStr); //2018-05-18 03:01:01 第2季 测试hook
 
 var dateObj = pureDateTime.dateObject(d);
 console.log(JSON.stringify(dateObj)) //{"M":5,"d":21,"h":11,"H":11,"m":54,"s":6,"q":2}
